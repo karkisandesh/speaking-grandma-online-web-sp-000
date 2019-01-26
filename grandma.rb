b3097a02d57daa835d7806e30d7436420b6f7970
Sandesh Karki <karkisandesh@hotmail.com>
2019-01-26 21:27:30 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma(phrase)
     if phrase.upcase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-    elsif phrase == " ".phrase
+    elsif " ".upcase == phrase
     "NO, NOT SINCE 1938!"
     else 
      "HUH?! SPEAK UP, SONNY!"
